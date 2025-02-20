8b22ca8f832f67328782fae41c91440144037a5e
Abraham-561 <fernandoesp561@gmail.com>
2025-02-20 12:41:28 -0600

    ya con 80 porciento avanzado

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -2,17 +2,23 @@ import { Router } from "express";
 import { UserRouter } from "../presentation/user/user.routes";
 
 
+
+import { PinRouter } from "../presentation/pin/pin.routes";import { SecurityBoxRouter } from "./security-box/security-box.routes";
+ 
+
 export class AppRoutes {
     static get routes(): Router {
-        const router = Router()
+        const router = Router();
 
-        router.use("/users", UserRouter.routes);
+        router.use("/users", UserRouter.routes); // Rutas de usuarios
+        router.use("/pins", PinRouter.routes);   // Rutas de PINs
+        //router.use("/credential", CredentialRouter.routes); // Rutas de credenciales
+        router.use("/security",    SecurityBoxRouter.routes); // Rutas de seguridad
+
         return router;
-
-
-
-
     }
 }
+
+
 
 
